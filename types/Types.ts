ff95682eab85685956b2b45e96b318656195135f
Kento75 <kento2github@gmail.com>
2021-10-05 07:43:32 +0900

    Add: fetch関数、ブログページ実装

---
+++ types/Types.ts
@@ -0,0 +1,26 @@
+/**
+ * フェイクエンドポイントの型定義
+ * https://jsonplaceholder.typicode.com/
+ */
+
+export interface POST {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+export interface COMMENT {
+  postId: number
+  id: number
+  name: string
+  email: string
+  body: string
+}
+
+export interface TASK {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
